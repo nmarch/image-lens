23f3a55df1c28d84100d6e7cfba3677b3ed992f6
Nicky March <nmarch@vcd-ch204-14.eastern.ewu.edu>
2016-10-06 14:30:15 -0700

    Updated BETTER WORK

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,9 @@ console.log('OK USA!!');
 //test jQuery
 //$('img').hide();
 
-$('img').imageLens({ 
-    imsgeSrc: '/image-lens/img/watch-zoom.jpg',
-    borderColor: "#ffff31",  
-    lensSize: 600 
+$('img').imageLens({
+    imageSrc: 'image-lens/img/watch-zoom.jpg',
+    lensSize: 600,
+    borderColor: 'ffff31'
+    
 });
-
-
-
